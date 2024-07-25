62d30c4662138b2235ed9734a82b5e13029f772e
NicoTheRicoo <nicotherico@gmail.com>
2024-07-25 17:05:43 -0400

    done

--- script.js
+++ script.js
@@ -7,14 +7,15 @@ const openModal = document.getElementById("addBtn").addEventListener("click", ()
     modal.showModal();
 });
 const submitModal = document.getElementById("submitBtn").addEventListener("click", () =>{
-    addPrompt();
+    addBookToLibrary();
     modal.close();
 });
-
+const bookShelf = document.getElementById("book-shelf");
+bookShelf.addEventListener("click", removeBook);
 
 const mylibrary = [];
 
-function book(title, author, pages, read){
+function Book(title, author, pages, read){
     this.title = title;
     this.author = author; 
     this.pages = pages;
@@ -26,17 +27,53 @@ function addBookToLibrary(){
     let a = document.getElementById("author").value;
     let p = document.getElementById("pages").value;
     let test = document.getElementById("read");
-    let r = "";
-    if (test.checked == true){
-        r = "read";
-    }else{
-        r = "not read"
-    }
-    
-    mylibrary.push(new book(t, a, p, r));   
-    visualLibraryUpdate();
+    let r = test.checked ? true : false;
+    let newBookObj = new Book(t, a, p, r);
+    mylibrary.push(newBookObj);   
+    visualLibraryUpdate(newBookObj);
 }
 
-function visualLibraryUpdate(){
-    
+function visualLibraryUpdate(newBookObj){
+    let newBook = document.createElement("div");
+    newBook.classList.add("book");
+    bookShelf.appendChild(newBook);
+    newBook.style.backgroundColor = randomBackgroundColor();
+
+    newBook.innerHTML = `
+             <div class="book-title">${newBookObj.title}</div>
+                <div class="book-author">${newBookObj.author}</div>
+                <div class = "book-bottom">
+                    <div class="book-pages">${newBookObj.pages}</div>
+                    <label class = "read-label">Read<input class="readBtn" type = "checkbox" ${newBookObj.read ? 'checked' : ''}></label>
+                    <button class="book-remove">Remove book</button>
+                </div>
+            </div>
+    `
 }
+
+function removeBook(event) {
+    if (event.target.classList.contains("book-remove")) {
+        let bookToRemove = event.target.closest(".book");
+        if (bookToRemove) {
+            bookToRemove.remove();
+        }
+    }
+}
+
+
+function randomBackgroundColor(){
+ const randomNum = getRandomInt(6);
+switch(randomNum){
+    case 0: return "rgb(211, 86, 86)";
+    case 1: return "rgb(211, 209, 86)";
+    case 2: return "rgb(90, 211, 86)";
+    case 3: return "rgb(86, 207, 211)";
+    case 4: return "rgb(211, 86, 211)";
+    case 5: return "rgb(86, 109, 211)";
+}
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
